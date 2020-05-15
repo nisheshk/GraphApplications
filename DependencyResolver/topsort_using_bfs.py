3e63494a0d91f8aba4e9839809776f0aa7729375
Nishesh Kalakheti <nissessk@gmail.com>
2020-05-15 17:46:35 -0400

    Adds code comment and is more readable

--- DependencyResolver/topsort_using_bfs.py
+++ DependencyResolver/topsort_using_bfs.py
@@ -1,26 +1,31 @@
 """
 Author: Nishesh Kalakheti
-Date: Oblivious of the date, due to COVID-19 effect.
+Date: May 15, 2020 
 Goal: To resolve the dependency between scripts. Say I have few scripts and one script is dependent
-        on others. Then, we must find a way to run the scripts in the right order. So, I have used the
-        concept of topological sorting using the degrees of the node.
+        on others. Then, we must find a way to run the scripts in the right order such that all the
+        tasks are completed. This is solved by using topological sorting of the graph, where graph is
+        represented as a dicitionary(adjacency list of the scripts). The scripts are the nodes and its
+        depdendencies are the edges.
 """
 from collections import deque
 
 def dependency_resolver(dependency_dic):
     #Initialize all the incoming edges to the nodes to be zero
     #This is done as it covers all the nodes (including isolated nodes)
+    #O(V) time complexity, where V are the number of scripts(nodes/vertices)
     in_degree = {u:0 for u in dependency_dic}
 
     #Check the incoming edges to the node and increment the value of the edge
     #in the in_degree dictionary
+    #O(V+E) as we are traversing through all the nodes and its edges.
     for u in dependency_dic:
         for v in dependency_dic[u]:
             in_degree[v] += 1
 
+    Q = deque()
     #The nodes with 0 incoming edges should be precessed first
     #As they have no dependency, hence they should occur first
-    Q = deque()
+    #O(V); as in_degree contains all the nodes
     for nodes in in_degree:
         if in_degree[nodes] == 0:
             Q.appendleft(nodes)
@@ -32,6 +37,7 @@ def dependency_resolver(dependency_dic):
     #reduce the edge of those nodes value by 1 which was poiting to that node
     #If that node's edge whose value we have reduced by 1 becomes 0, append to the queue
     #Because they can be processed again, as their dependency is completed.
+    #O(V+E) If the graph is acyclic, we traverse through all the edges and its vertices.
     while Q:
         u = Q.pop()
         result.append(u)
@@ -49,9 +55,11 @@ def dependency_resolver(dependency_dic):
     
 
 
-
-dependency_resolver  = {"a.py":set(["d.py"]),"b.py":set(["a.py"]),"c.py":set(["f.py"]),"e.py":set(["c.py","d.py"]),"d.py":set(["c.py"]),"f.py":set([])}
-res = topsort(dependency_resolver)
+#The value of the dictionary depends on the key to be completed.
+#Example "e.py":set(["c.py","d.py"] means that only after e.py executes,
+#We can execute c.py and d.py
+scripts_depdendency  = {"a.py":set(["d.py"]),"b.py":set(["a.py"]),"c.py":set(["f.py"]),"e.py":set(["c.py","d.py"]),"d.py":set(["c.py"]),"f.py":set([])}
+res = dependency_resolver(scripts_depdendency)
 if res: #No cycle exists
     print (res)
 else:
